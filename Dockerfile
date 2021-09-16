eb9523aa1d73dc54432da33ad4f1a9ecac742744
Danny Mandel <danny.mandel@gmail.com>
2021-09-15 17:27:00 -0700

    Trying to get solr schema init going

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ WORKDIR /app
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
+
 COPY . .
-CMD [ "uvicorn", "isb_web.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD [ "uvicorn", "isb_web.main:app", "--host", "0.0.0.0", "--port", "8000" ]
--- solr_schema_init/create_isb_core_schema.py
+++ solr_schema_init/create_isb_core_schema.py
@@ -3,7 +3,7 @@ import typing
 
 import requests
 
-SOLR_API = "http://localhost:8983/api/collections/isb_core_records/"
+SOLR_API = "http://solr:8983/api/collections/isb_core_records/"
 MEDIA_JSON = "application/json"
 
 
@@ -86,8 +86,8 @@ def addDynamicField(dynamic_field_dict: typing.Dict):
 
 #############
 # Internal iSamples bookkeeping columns
-# createField("isb_core_id", "string", True, True, None)
-# createField("source", "string", True, True, None)
+createField("isb_core_id", "string", True, True, None)
+createField("source", "string", True, True, None)
 # The time the record was last updated in the source db
 createField("sourceUpdatedTime", "pdate", True, True, None)
 # The time the record was last updated in the iSamples index
@@ -95,83 +95,81 @@ createField("indexUpdatedTime", "pdate", True, True, None)
 #############
 
 
-# createField("label", "string", True, True, None)
-# createField("description", "string", True, True, None)
-# createField("description_text", "text_general", True, True, None)
-# createCopyField("description", "description_text")
-# createField("hasContextCategory", "string", True, True, None, True)
-# createField("hasMaterialCategory", "string", True, True, None, True)
-# createField("hasSpecimenCategory", "string", True, True, None, True)
-# createField("keywords", "string", True, True, None, True)
-# createField("informalClassification", "string", True, True, None, True)
-# createField("producedBy_isb_core_id", "string", True, True, None)
-# createField("producedBy_label", "string", True, True, None)
-# createField("producedBy_description", "string", True, True, None)
-# createField("producedBy_description_text", "text_general", True, True, None)
-# createCopyField("producedBy_description", "producedBy_description_text")
-# createField("producedBy_hasFeatureOfInterest", "string", True, True, None)
-# createField("producedBy_responsibility", "string", True, True, None, True)
-# createField("producedBy_resultTime", "pdate", True, True, None)
-# createField("producedBy_samplingSite_description", "string", True, True, None)
-# createField(
-#     "producedBy_samplingSite_description_text", "text_general", True, True, None
-# )
-# createCopyField("producedBy_samplingSite_description", "producedBy_samplingSite_description_text")
-# createField("producedBy_samplingSite_label", "string", True, True, None)
-# createField("producedBy_samplingSite_location_elevationInMeters", "pfloat", True, True, None)
-# createField("producedBy_samplingSite_location_latlon", "location", True, True, None)
-# createField("producedBy_samplingSite_placeName", "string", True, True, None, True)
-# createField("registrant", "string", True, True, None, True)
-# createField("samplingPurpose", "string", True, True, None, True)
-# createField("curation_label", "string", True, True, None)
-# createField("curation_description", "string", True, True, None)
-# createField("curation_description_text", "text_general", True, True, None)
-# createCopyField("curation_description", "curation_description_text")
-# createField("curation_accessContraints", "string", True, True, None)
-# createField("curation_location", "string", True, True, None)
-# createField("curation_responsibility", "string", True, True, None)
-# createField("relatedResource_isb_core_id", "string", True, True, None, True)
-#
-# pj(listFields())
+createField("label", "string", True, True, None)
+createField("description", "string", True, True, None)
+createField("description_text", "text_general", True, True, None)
+createCopyField("description", "description_text")
+createField("hasContextCategory", "string", True, True, None, True)
+createField("hasMaterialCategory", "string", True, True, None, True)
+createField("hasSpecimenCategory", "string", True, True, None, True)
+createField("keywords", "string", True, True, None, True)
+createField("informalClassification", "string", True, True, None, True)
+createField("producedBy_isb_core_id", "string", True, True, None)
+createField("producedBy_label", "string", True, True, None)
+createField("producedBy_description", "string", True, True, None)
+createField("producedBy_description_text", "text_general", True, True, None)
+createCopyField("producedBy_description", "producedBy_description_text")
+createField("producedBy_hasFeatureOfInterest", "string", True, True, None)
+createField("producedBy_responsibility", "string", True, True, None, True)
+createField("producedBy_resultTime", "pdate", True, True, None)
+createField("producedBy_samplingSite_description", "string", True, True, None)
+createField(
+    "producedBy_samplingSite_description_text", "text_general", True, True, None
+)
+createCopyField("producedBy_samplingSite_description", "producedBy_samplingSite_description_text")
+createField("producedBy_samplingSite_label", "string", True, True, None)
+createField("producedBy_samplingSite_location_elevationInMeters", "pfloat", True, True, None)
+createField("producedBy_samplingSite_placeName", "string", True, True, None, True)
+createField("registrant", "string", True, True, None, True)
+createField("samplingPurpose", "string", True, True, None, True)
+createField("curation_label", "string", True, True, None)
+createField("curation_description", "string", True, True, None)
+createField("curation_description_text", "text_general", True, True, None)
+createCopyField("curation_description", "curation_description_text")
+createField("curation_accessContraints", "string", True, True, None)
+createField("curation_location", "string", True, True, None)
+createField("curation_responsibility", "string", True, True, None)
+createField("relatedResource_isb_core_id", "string", True, True, None, True)
 
-# replaceFieldType({
-#     "name": "location_rpt",
-#     "class": "solr.SpatialRecursivePrefixTreeFieldType",
-#     "geo": True,
-#     "omitNorms": True,
-#     "omitTermFreqAndPositions": True,
-#     "spatialContextFactory": "JTS",
-#     "termOffsets": False,
-#     "termPositions": False,
-#     "omitPositions": True,
-#     "autoIndex": True
-# })
-# addFieldType({
-#     "name": "bbox",
-#     "class": "solr.BBoxField",
-#     "geo": True,
-#     "numberType": "pdouble",
-#     "distanceUnits": "kilometers"
-# })
-# addDynamicField({
-#     "name": "*_ll",
-#     "type": "location",
-#     "indexed": True,
-#     "stored": True
-# })
-# addDynamicField({
-#     "name":"*_bb",
-#     "type":"bbox"
-# })
-# addDynamicField({
-#     "name":"*_rpt",
-#     "type":"location_rpt",
-#     "multiValued":True,
-#     "indexed":True,
-#     "stored":True
-# })
+replaceFieldType({
+    "name": "location_rpt",
+    "class": "solr.SpatialRecursivePrefixTreeFieldType",
+    "geo": True,
+    "omitNorms": True,
+    "omitTermFreqAndPositions": True,
+    "spatialContextFactory": "JTS",
+    "termOffsets": False,
+    "termPositions": False,
+    "omitPositions": True,
+    "autoIndex": True
+})
+addFieldType({
+    "name": "bbox",
+    "class": "solr.BBoxField",
+    "geo": True,
+    "numberType": "pdouble",
+    "distanceUnits": "kilometers"
+})
+addDynamicField({
+    "name": "*_ll",
+    "type": "location",
+    "indexed": True,
+    "stored": True
+})
+addDynamicField({
+    "name":"*_bb",
+    "type":"bbox"
+})
+addDynamicField({
+    "name":"*_rpt",
+    "type":"location_rpt",
+    "multiValued":True,
+    "indexed":True,
+    "stored":True
+})
 
-#deleteField("producedBy_samplingSite_location_latlon")
-#createField("producedBy_samplingSite_location_ll", "location", True, True, None)
-#createField("producedBy_samplingSite_location_bb", "bbox", True, True, None)
-#createField("producedBy_samplingSite_location_rpt", "location_rpt", True, True, None)
+createField("producedBy_samplingSite_location_ll", "location", True, True, None)
+createField("producedBy_samplingSite_location_bb", "bbox", True, True, None)
+createField("producedBy_samplingSite_location_rpt", "location_rpt", True, True, None)
+
+pj(listFields())
