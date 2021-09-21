7d8b8248b0e5f3039268677d5b52370a941fabb6
Danny Mandel <danny.mandel@gmail.com>
2021-09-21 10:47:40 -0700

    Undo changes to config class now that config file is working as expected

--- isb_web/config.py
+++ isb_web/config.py
@@ -1,9 +1,9 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql+psycopg2://isb_writer:isamplesinabox@db/isb_1"
+    database_url: str = "UNSET"
     web_root: str = "/"
-    solr_url: str = "http://solr:8983/solr/isb_core_records"
+    solr_url: str = "UNSET"
 
     class Config:
         env_file = "isb_web_config.env"
