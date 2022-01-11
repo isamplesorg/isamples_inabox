c007dcfc9870443cfca599b5865c8d4da7ad88fb
Danny Mandel <danny.mandel@gmail.com>
2022-01-11 11:01:10 -0700

    Pagination test

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -6,6 +6,7 @@ from jsonschema import validate
 
 SOLR_items = [
     "./test_data/isb_core_solr_documents/ark-21547-Dob2MVZ_HERP_266812.json",
+    "./test_data/isb_core_solr_documents/ark-21547-BHP2CFR_368.json",
 ]
 
 
@@ -41,3 +42,23 @@ def test_stac_collection_valid(solr_file_path, stac_collection_schema_json):
         )
         assert stac_collection is not None
         validate(instance=stac_collection, schema=stac_collection_schema_json)
+
+
+def _contains_next_link(links: typing.List[typing.Dict]) -> bool:
+    next_links = [link for link in links if link["rel"] == "next"]
+    return len(next_links) > 0
+
+
+def test_stac_collection_pagination():
+    with open(SOLR_items[0]) as source_file_zero:
+        solr_document_zero = json.load(source_file_zero)
+        # verify that the first "collection" has a pointer to the next
+        stac_collection = isb_lib.stac.stac_collection_from_solr_dicts([solr_document_zero], True, 0, 1, None)
+        links = stac_collection["links"]
+        assert _contains_next_link(links) is True
+    with open(SOLR_items[1]) as source_file_one:
+        solr_document_one = json.load(source_file_one)
+        # verify that the second "collection" has no pointer to the next
+        stac_collection = isb_lib.stac.stac_collection_from_solr_dicts([solr_document_one], False, 0, 1, None)
+        links = stac_collection["links"]
+        assert _contains_next_link(links) is False
