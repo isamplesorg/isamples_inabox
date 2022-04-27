4b09b5cfa23fac43469768cf920e913c67c44ee2
Danny Mandel <danny.mandel@gmail.com>
2022-04-27 10:24:55 -0700

    Handle nulls appropriately so this may be run every week

--- scripts/migrations/populate_h3_for_existing_things.py
+++ scripts/migrations/populate_h3_for_existing_things.py
@@ -45,18 +45,14 @@ def assign_h3(session: Session):
         isb_lib.opencontext_adapter.OpenContextItem.AUTHORITY_ID: OpenContextTransformer.geo_to_h3,
     }
     for authority, geo_to_h3 in authorities.items():
-        thing_select = (
-            select(Thing.primary_key, Thing.resolved_content)
-            .limit(BATCH_SIZE)
-            .order_by(Thing.primary_key.asc())
-            .filter(Thing.authority_id == authority)
-        )
+        thing_select = things_no_h3_select(authority)
         print(f"Starting h3 migration for {authority}")
         i = 0
         update_batch_values = []
         last_id = None
         while True:
             if last_id is not None:
+                thing_select = things_no_h3_select(authority)
                 thing_select = thing_select.filter(Thing.primary_key > last_id)
             results = session.exec(thing_select).all()
             if len(results) == 0:
@@ -65,12 +61,27 @@ def assign_h3(session: Session):
             for row in results:
                 i += 1
                 primary_key = row[0]
-                update_batch_values.append(
-                    {"primary_key": primary_key, "h3": geo_to_h3(row[1])}
-                )
+                h3 = geo_to_h3(row[1])
+                if h3 is not None:
+                    update_batch_values.append({"primary_key": primary_key, "h3": h3})
                 last_id = primary_key
-            save_batch(session, update_batch_values)
+            # It's possible the length could be 0 since we don't have lat/lon for all records
+            if len(update_batch_values) > 0:
+                save_batch(session, update_batch_values)
+            else:
+                print("Skipping update batch as all h3 values were None")
             update_batch_values = []
+
+
+def things_no_h3_select(authority):
+    thing_select = (
+        select(Thing.primary_key, Thing.resolved_content)
+        .limit(BATCH_SIZE)
+        .order_by(Thing.primary_key.asc())
+        .filter(Thing.authority_id == authority)
+        .filter(Thing.h3 == None)  # noqa: E711
+    )
+    return thing_select
 
 
 def save_batch(session, update_batch_values):
