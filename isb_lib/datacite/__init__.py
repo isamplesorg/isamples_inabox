527805382f4cd5fa63daf8cc7d2e344c3a4acfb5
Danny Mandel <danny.mandel@gmail.com>
2022-05-06 15:20:52 -0700

    Datacite initial experiment

---
+++ isb_lib/datacite/__init__.py
@@ -0,0 +1,39 @@
+import json
+import typing
+import logging
+import requests
+from requests.auth import HTTPBasicAuth
+
+import isb_web.config
+
+CONTENT_TYPE = "application/vnd.api+json"
+DATACITE_URL = "https://api.test.datacite.org"
+
+
+def create_draft_doi(
+    rsession: requests.session,
+    prefix: str,
+    username: str,
+    password: str,
+) -> typing.Dict:
+    dois_url = f"{DATACITE_URL}/dois"
+    headers = {"content-type": CONTENT_TYPE}
+    request_data = {
+        "data": {
+            "type": "dois",
+            "attributes": {
+                "prefix": prefix
+            }
+        },
+    }
+    post_data_str = json.dumps(request_data).encode("utf-8")
+    auth = HTTPBasicAuth(username, password)
+    response = rsession.post(
+        dois_url, headers=headers, data=post_data_str, auth=auth
+    )
+    if response.status_code != 200:
+        logging.error(
+            "Error requesting new DOI, status code: %s",
+            response.status_code,
+        )
+    return response.json()
