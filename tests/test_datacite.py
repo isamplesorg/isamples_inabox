c6c9347648d436e8cd496dfdfcad184523b4a2a5
Danny Mandel <danny.mandel@gmail.com>
2022-05-18 08:32:24 -0700

    Fix tests

--- tests/test_datacite.py
+++ tests/test_datacite.py
@@ -10,7 +10,7 @@ def test_datacite_metadata():
         source_record = json.load(source_file)
         prefix = "123456"
         authority = "AUTHORITY"
-        doi_metadata = datacite.datacite_metadata_from_core_record(prefix, None, authority, source_record)
+        doi_metadata = datacite.datacite_metadata_from_core_record(prefix, None, False, authority, source_record)
         assert doi_metadata is not None
         data_dict = doi_metadata.get("data")
         assert data_dict is not None
@@ -27,4 +27,4 @@ def test_datacite_metadata_missing_label():
     with open(test_file_path) as source_file:
         source_record = json.load(source_file)
         with pytest.raises(ValueError):
-            datacite.datacite_metadata_from_core_record("prefix", None, "authority", source_record)
+            datacite.datacite_metadata_from_core_record("prefix", None, False, "authority", source_record)
