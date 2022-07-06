b72388e79043fd5ebdd26a48ea3f44798e4a58a9
Danny Mandel <danny.mandel@gmail.com>
2022-07-05 17:00:19 -0700

    mypy and flake8 fixes

--- isb_lib/utilities/url_utilities.py
+++ isb_lib/utilities/url_utilities.py
@@ -12,9 +12,9 @@ def last_path_component(url: URL) -> str:
 
 def joined_url(original_url: str, fragment_to_join: str) -> str:
     logging.debug(f"url to join is {original_url}")
-    original_url = parse.urlparse(original_url)
-    logging.debug(f"url path is {original_url.path}")
-    base = original_url.scheme + "://" + original_url.netloc
+    parse_result = parse.urlparse(original_url)
+    logging.debug(f"url path is {parse_result.path}")
+    base = parse_result.scheme + "://" + parse_result.netloc
     joined = urljoin(base, fragment_to_join)
     logging.debug(f"joined url is {joined}")
     return joined
