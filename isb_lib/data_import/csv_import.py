d6b447d27ecd1b9c78f66acb266c2d8f78069077
Danny Mandel <danny.mandel@gmail.com>
2022-10-05 13:25:22 -0700

    mypy fixes

--- isb_lib/data_import/csv_import.py
+++ isb_lib/data_import/csv_import.py
@@ -1,7 +1,7 @@
 import datetime
 from typing import Optional
 
-import isamples_frictionless
+from isamples_frictionless import isamples_frictionless
 from frictionless import Package, Resource
 from sqlmodel import Session
 
