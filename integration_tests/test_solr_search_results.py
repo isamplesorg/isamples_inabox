65d7b5b017142c98fab6dbd438eaf69628e7f5da
Danny Mandel <danny.mandel@gmail.com>
2022-10-14 15:43:33 -0700

    Get skeleton of integration test in place

--- integration_tests/test_solr_search_results.py
+++ integration_tests/test_solr_search_results.py
@@ -39,7 +39,7 @@ def _send_solr_query(
         "start": 0,
         "limit": 10,
         "fl": "*",
-        "q": f"searchText:({query})",
+        "q": query,
     }
     res = rsession.get(solr_url, headers=headers(), params=params)
     response_dict = res.json()
@@ -56,11 +56,15 @@ def _value_in_search_text(value: str, doc: typing.Dict) -> bool:
     return appears_in_search_text
 
 
+def search_text_query(search_text: str) -> str:
+    return f"searchText:({search_text})"
+
+
 @pytest.mark.parametrize("authority_id", authority_ids)
 def test_solr_query_by_source(
     rsession: requests.Session, solr_url: str, authority_id: str
 ):
-    docs = _send_solr_query(rsession, solr_url, authority_id)
+    docs = _send_solr_query(rsession, solr_url, search_text_query(authority_id))
     for doc in docs:
         appears_in_search_text = _value_in_search_text(authority_id, doc)
         assert appears_in_search_text
@@ -81,7 +85,7 @@ opencontext_projects = [
 def test_opencontext_projects(
     rsession: requests.Session, solr_url: str, project_label: str
 ):
-    docs = _send_solr_query(rsession, solr_url, project_label)
+    docs = _send_solr_query(rsession, solr_url, search_text_query(project_label))
     for doc in docs:
         for word in project_label.split():
             appears_in_search_text = _value_in_search_text(word, doc)
@@ -101,7 +105,7 @@ geome_search_terms = [
 def test_geome_search_terms(
     rsession: requests.Session, solr_url: str, geome_search_term: str
 ):
-    docs = _send_solr_query(rsession, solr_url, geome_search_term)
+    docs = _send_solr_query(rsession, solr_url, search_text_query(geome_search_term))
     for doc in docs:
         for word in geome_search_term.split():
             appears_in_search_text = _value_in_search_text(word, doc)
@@ -122,7 +126,38 @@ geographic_search_terms = [
 def test_geographic_search_terms(
     rsession: requests.Session, solr_url: str, geographic_search_term: str
 ):
-    docs = _send_solr_query(rsession, solr_url, geographic_search_term)
+    docs = _send_solr_query(rsession, solr_url, search_text_query(geographic_search_term))
     for doc in docs:
         appears_in_search_text = _value_in_search_text(geographic_search_term, doc)
         assert appears_in_search_text
+
+
+solr_test_values = [
+    (
+        "IGSN:NHB002GWT",
+        {
+            "hasContextCategory": ["Earth interior"],
+            "hasMaterialCategory": ["Mineral"],
+            "hasSpecimenCategory": ["Other solid object"]
+        }
+    ),
+    (
+        "IGSN:NHB002GWU",
+        {
+            "hasContextCategory": ["Earth interior"],
+            "hasMaterialCategory": ["Mineral"],
+            "hasSpecimenCategory": ["Other solid object"]
+        }
+    )
+]
+
+
+@pytest.mark.skipif(True, reason="Only run this test manually, not intended to be automated.  Manually flip the True to False to run.")
+@pytest.mark.parametrize("id,params", solr_test_values)
+def test_solr_integration_test(rsession: requests.Session, solr_url: str, id: str, params: dict):
+    solr_query = f"id:\"{id}\""
+    docs = _send_solr_query(rsession, solr_url, solr_query)
+    assert len(docs) > 0
+    test_doc = docs[0]
+    for key, value in params.items():
+        assert test_doc.get(key) == value
