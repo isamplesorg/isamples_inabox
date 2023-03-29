6ada1394b5153da5fd7530e321dede3ee6860a12
Danny Mandel <danny.mandel@gmail.com>
2023-03-29 10:48:38 -0700

    Initial implementation of GBIF taxon name cache (#257)
    * Initial implementation of GBIF taxon name cache
    
    * Database access for the cache and tests to exercise it
    
    * Migration script to add GEOME context categories to solr index
    
    * flake8
    
    * More flake8

--- isb_web/sqlmodel_database.py
+++ isb_web/sqlmodel_database.py
@@ -15,6 +15,7 @@ from sqlmodel.sql.expression import SelectOfScalar
 
 import isb_lib
 from isb_lib.models.person import Person
+from isb_lib.models.taxonomy_name import TaxonomyName
 from isb_lib.models.thing import Thing, ThingIdentifier, Point
 from isb_web.schemas import ThingPage
 
@@ -497,3 +498,18 @@ def load_things_from_file(session: Session, file_path: str):
     sql = f"copy thing ({THING_EXPORT_FIELD_LIST}) from '{file_path}'"
     session.execute(sql)
     session.commit()
+
+
+def save_taxonomy_name(session: Session, name: TaxonomyName, commit: bool = False):
+    session.add(name)
+    if commit:
+        session.commit()
+
+
+def taxonomy_name_to_kingdom_map(session: Session) -> dict:
+    name_select = select(TaxonomyName.name, TaxonomyName.kingdom)
+    name_rows = session.execute(name_select).fetchall()
+    name_dict = {}
+    for row in name_rows:
+        name_dict[row[0]] = row[1]
+    return name_dict
