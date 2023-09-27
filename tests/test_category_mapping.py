95cac658d802cf65ec99ec75b51149e5ec27388e
Danny Mandel <danny.mandel@gmail.com>
2023-09-27 15:07:00 -0700

    Update tests to new format

--- tests/test_category_mapping.py
+++ tests/test_category_mapping.py
@@ -5,15 +5,16 @@ from isamples_metadata.Transformer import (
     StringOrderedCategoryMapper,
     StringEndsWithCategoryMapper,
 )
+from isamples_metadata.vocabularies import vocabulary_mapper
 
 
 @pytest.fixture
 def soil_mapper():
     endsWithSoilMapper = StringEndsWithCategoryMapper(
-        "Soil", "Subaerial surface environment"
+        "Soil", "Subaerial surface environment", vocabulary_mapper.SAMPLED_FEATURE
     )
     soilFloodplainMapper = StringPairedCategoryMapper(
-        "Microbiology>Soil", "floodplain", "Subaerial surface environment"
+        "Microbiology>Soil", "floodplain", "Subaerial surface environment", vocabulary_mapper.SAMPLED_FEATURE
     )
     soilMapper = StringOrderedCategoryMapper(
         # Order matters here, the generic one needs to be last
@@ -26,18 +27,18 @@ def test_compound_categories(soil_mapper):
     # For the case of the specific pair, we should get the specific match
     categories = []
     soil_mapper.append_if_matched("Microbiology>Soil", "floodplain", categories)
-    assert categories[0] == "Subaerial surface environment"
+    assert categories[0].label == "Subaerial surface environment"
 
 
 def test_plain_soil(soil_mapper):
     # location doesn't match the specific one we care about, get generic
     categories = []
     soil_mapper.append_if_matched("Soil", "boreal forest", categories)
-    assert categories[0] == "Subaerial surface environment"
+    assert categories[0].label == "Subaerial surface environment"
 
 
 def test_ends_with_soil(soil_mapper):
     # no location, get generic
     categories = []
     soil_mapper.append_if_matched("Metamorphic>Soil", "", categories)
-    assert categories[0] == "Subaerial surface environment"
+    assert categories[0].label == "Subaerial surface environment"
