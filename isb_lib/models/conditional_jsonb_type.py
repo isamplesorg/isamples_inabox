cf706be4366d6095170dcd66c3917f6ec3950831
Danny Mandel <danny.mandel@gmail.com>
2023-10-17 15:11:09 -0700

    Add link to documentation where we got the code from

--- isb_lib/models/conditional_jsonb_type.py
+++ isb_lib/models/conditional_jsonb_type.py
@@ -4,6 +4,7 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 class ConditionalJSONB(TypeDecorator):
     """Type that will use JSONB when connected to postgresql, and JSON otherwise.
+    Modeled after example at: https://docs.sqlalchemy.org/en/20/core/custom_types.html#sqlalchemy.types.TypeDecorator.load_dialect_impl
     """
 
     impl = JSON
