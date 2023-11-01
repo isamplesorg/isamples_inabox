bb41e2811a69a42148b682c6192a60412b204f30
Danny Mandel <danny.mandel@gmail.com>
2023-11-01 12:10:26 -0700

    Update integration tests to point at the new server

--- integration_tests/test_sitemaps_integration_test.py
+++ integration_tests/test_sitemaps_integration_test.py
@@ -16,7 +16,7 @@ def rsession():
 def sitemap_index_url():
     sitemap_index_url = os.getenv("INPUT_SITEMAP_INDEX_URL")
     if sitemap_index_url is None:
-        sitemap_index_url = "https://hyde.cyverse.org/sitemaps/sitemap-index.xml"
+        sitemap_index_url = "https://central.isample.xyz/sitemaps/sitemap-index.xml"
     return sitemap_index_url
 
 
