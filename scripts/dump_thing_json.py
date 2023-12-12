7f9d542ad3b033a82efc4c3600addfab1a2fbeac
Danny Mandel <danny.mandel@gmail.com>
2023-12-12 11:32:21 -0700

    Include test that was written and never checked in, also include tool for dumping json files to a disk

---
+++ scripts/dump_thing_json.py
@@ -0,0 +1,57 @@
+import json
+import os.path
+
+import click
+
+import isb_lib.core
+from isb_web.sqlmodel_database import SQLModelDAO, paged_things_with_ids
+
+
+@click.command()
+@click.option(
+    "-d", "--db_url", default=None, help="SQLAlchemy database URL for storage"
+)
+@click.option(
+    "-v",
+    "--verbosity",
+    default="DEBUG",
+    help="Specify logging level",
+    show_default=True,
+)
+@click.option(
+    "-a",
+    "--authority",
+    default="SMITHSONIAN",
+    help="Which authority to use when selecting and dumping the resolved_content",
+)
+@click.option(
+    "-c",
+    "--count",
+    default=10000,
+    help="The number of things to dump",
+)
+@click.option(
+    "-p",
+    "--path",
+    default=None,
+    help="The path to write out the JSON files",
+)
+@click.pass_context
+def main(ctx, db_url: str, verbosity: str, authority: str, count: int, path: str):
+    """Program to dump the specified number of things to the specified path"""
+    isb_lib.core.things_main(ctx, db_url, None, verbosity)
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print(f"Created directory at {path}")
+    session = SQLModelDAO((db_url)).get_session()
+    things = paged_things_with_ids(session, authority, 200, count)
+    for thing in things:
+        id = thing.id.replace("http://opencontext.org/subjects/", "")
+        id = id.replace("https://opencontext.org/subjects/", "")
+        dest_path = os.path.join(path, f"{id}.json")
+        with open(dest_path, "w") as json_file:
+            json.dump(thing.resolved_content, json_file)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_isb_solr_query.py
@@ -0,0 +1,7 @@
+from isb_web.isb_solr_query import _solr_heatmap_geom_params_str, MIN_LAT, MAX_LAT, MIN_LON, MAX_LON
+
+
+def test_solr_heat_geom_params_str():
+    bb = {MIN_LAT: -90.0, MAX_LAT: 90.0, MIN_LON: -180.0, MAX_LON: 180.0}
+    params_str = _solr_heatmap_geom_params_str(bb)
+    assert "[-180.0 -90.0 TO 180.0 90.0]" == params_str
