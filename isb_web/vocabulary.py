9bbba272fd7b04bfcfb193ff7fe92c58d7ff4177
Danny Mandel <danny.mandel@gmail.com>
2024-02-07 12:16:34 -0700

    Write test for the vocabulary handler

--- isb_web/vocabulary.py
+++ isb_web/vocabulary.py
@@ -2,9 +2,9 @@ import logging
 from typing import Optional
 
 import term_store
+from term_store import TermRepository
 from fastapi import APIRouter, Depends
 from sqlmodel import Session
-
 from isb_lib.vocabulary import vocab_adapter
 from isb_web.sqlmodel_database import SQLModelDAO
 
@@ -19,19 +19,20 @@ def get_session():
         yield session
 
 
+def get_repository(session: Session = Depends(get_session)) -> TermRepository:
+    return term_store.get_repository(session)
+
+
 @router.get("/material_sample_type")
-def material_sample_type(session: Session = Depends(get_session)) -> dict:
-    repository = term_store.get_repository(session)
+def material_sample_type(repository: TermRepository = Depends(get_repository)) -> dict:
     return vocab_adapter.uijson_vocabulary_dict("https://w3id.org/isample/vocabulary/specimentype/1.0/physicalspecimen", repository)
 
 
 @router.get("/material_type")
-def material_type(session: Session = Depends(get_session)) -> dict:
-    repository = term_store.get_repository(session)
+def material_type(repository: TermRepository = Depends(get_repository)) -> dict:
     return vocab_adapter.uijson_vocabulary_dict("https://w3id.org/isample/vocabulary/material/1.0/material", repository)
 
 
 @router.get("/sampled_feature_type")
-def sampled_feature_type(session: Session = Depends(get_session)) -> dict:
-    repository = term_store.get_repository(session)
+def sampled_feature_type(repository: TermRepository = Depends(get_repository)) -> dict:
     return vocab_adapter.uijson_vocabulary_dict("https://w3id.org/isample/vocabulary/sampledfeature/1.0/anysampledfeature", repository)
